7cb5783197aba385e7f94883a192697985b1a360
Thomas Stilwell <stilwellt@openlabs.co>
2014-09-12 03:19:15 -0500

    stop.sh: wait intelligently after kill instead of sleeping 5 seconds
    Signed-off-by: Thomas Stilwell <stilwellt@openlabs.co>

--- stop.sh
+++ stop.sh
@@ -2,10 +2,18 @@
 for pid in `ps aux | grep "python.*openbazaar_daemon.py" | grep -v grep | awk '{print $2}'`; do
   echo "Sending SIGTERM to ${pid}"
   kill ${pid}
-  sleep 5s
-  if ps -p $pid > /dev/null
-    then
-    echo "Still running, sending SIGKILL to ${pid}"
-    kill -9 ${pid}
-  fi
+  TRIES=50
+  while [ 1 ]; do
+    if ps -p $pid > /dev/null; then
+      let TRIES-=1
+      { usleep 100000 || sleep 0.1; } &>/dev/null
+    else
+      break
+    fi
+    if [ $TRIES -eq 0 ]; then
+      echo "Still running, sending SIGKILL to ${pid}"
+      kill -9 ${pid}
+      break
+    fi
+  done
 done
